9f3ee9192d9415a4b4e200000b5ec2c6300d617f
Covarel <covarelcamille@gmail.com>
2025-08-07 22:27:21 +0200

    LandingPage.tsx small fix

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -25,6 +25,7 @@ function LandingPage() {
           <AnamnesisLogo className="hero-logo" aria-label="Logo Anamnesis" />
           <p className="subtitle">
             <strong>Anamnesis</strong> est un{" "}
+            <strong>framework full-stack</strong> moderne pour se lancer sans s'égarer :)
           </p>
           <div className="cta-buttons">
             <a
